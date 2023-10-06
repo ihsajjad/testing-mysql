b188d280f0ba7c7baee43b7370b1778f10ef2925
ihsajjad <ihsajjad1@gmail.com>
2023-10-07 03:45:42 +0600

    changed the port name

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3306;
 
 // middleware
 app.use(express.json());
